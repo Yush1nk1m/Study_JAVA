3668020c838e9b5b0da5316fdf657a814ef7d405
Yush1nk1m <kys010306@sogang.ac.kr>
2022-10-04 00:20:50 +0900

    20221004

---
+++ chapter09/src/sec01/verify/exam05/BackgroundChangeListener.java
@@ -0,0 +1,7 @@
+package sec01.verify.exam05;
+
+public class BackgroundChangeListener implements CheckBox.OnSelectListener {
+	public void onSelect() {
+		System.out.println("Change the background.");
+	}
+}
